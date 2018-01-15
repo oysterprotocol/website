d12a34721c63255edd1278be37cbe95a4be7c061
Rajat <rajats.1709@gmail.com>
2018-01-15 18:29:43 -0500

    added gitignore & changed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var gulp = require('gulp'),
+const gulp = require('gulp'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
